1dc34de9e0d3ba35acadbf31080920d14cb09dd1
zafergunayonalan <zafergunay7@gmail.com>
2025-05-24 00:23:16 +0300

    create-Pages

---
+++ travel/src/app/(routes)/rent-a-cars/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const RentacarPage = () => {
+  return (
+    <div>RentacarPage</div>
+  )
+}
+
+export default RentacarPage
---
+++ travel/src/app/(routes)/trips/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TripsPage = () => {
+  return (
+    <div>TripsPage</div>
+  )
+}
+
+export default TripsPage
